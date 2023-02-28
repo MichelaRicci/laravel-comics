73c06032aca96a7aa6ae46bbb68ce84b99ca62d3
MichelaRicci <michela.ricci.1991@gmail.com>
2023-02-28 13:23:46 +0100

    adjustment header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,4 +23,29 @@
             </div>
         </div>
     </section>
+
+    <section id="shop-section">
+        <div class="container d-flex">
+            <div class="shop-card">
+                <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="buy-comics-digital-comics.png">
+                <div class="text-uppercase text-white ps-3 align-self-center">Digital Comics</div>
+            </div>
+            <div class="shop-card">
+                <img class="img-fluid"  src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="buy-comics-merchandise">
+                <div class="text-uppercase text-white ps-3 align-self-center">DC Merchandise </div>
+            </div>
+            <div class="shop-card">
+                <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="buy-comics-subscriptions.png">
+                <div class="text-uppercase text-white ps-3 align-self-center">Subscription </div>
+            </div>
+            <div class="shop-card">
+                <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="/buy-comics-shop-locator.png">
+                <div class="text-uppercase text-white ps-3 align-self-center">Comic Shop Locator </div>
+            </div>
+            <div class="shop-card">
+                <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="buy-dc-power-visa.svg">
+                <div class="text-uppercase text-white ps-3 align-self-center">DC Power Visa </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,14 +1,28 @@
+@php 
+
+$menu = ['characters', 'comics', 'movies', 'tv', 'games', 'collectibles', 'videos', 'fans', 'news', 'shop']
+
+@endphp
+
+
+
 
 <header>
 
-    <nav class="container">
+    <nav class="container justify-content-around align-items-center">
         <div class="nav-left">
             <a href="{{ url('/') }}">
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC">
+                <img id="logo-header" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo DC">
             </a>
         </div>
         <div class="nav-right d-flex">
-            <ul class="d-flex">
+            @foreach ($menu as $menu)
+             <ul>
+                <li><a class="text-uppercase fw-bold" href="{{$menu}}">{{ $menu }}</a></li>
+            </ul>
+            @endforeach
+
+            <!-- <ul class="fw-bold">
                 <li><a href="{{ Route('characters') }}" class="text-uppercase">Characters</a></li>
                 <li><a href="{{ Route('comics') }}" class="text-uppercase">Comics</a></li>
                 <li><a href="{{ Route('movies') }}" class="text-uppercase">Movies</a></li>
@@ -19,8 +33,8 @@
                 <li><a href="{{ Route('fans') }}" class="text-uppercase">Fans</a></li>
                 <li><a href="{{ Route('news') }}" class="text-uppercase">News</a></li>
                 <li><a href="{{ Route('shop') }}" class="text-uppercase">Shop</a></li>
-            </ul>
-            <div><input type="text" class="mt-4"></div>
+            </ul> -->
+            <div><input id="searchbar" type="text"  placeholder="Search" class="mt-4"></div>
 
 
         </div>
